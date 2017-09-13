6b614073b8789ce0de661ab7151a00b09928235c
jonfinley <accounts@jonathanfinley.net>
2017-09-13 15:40:16 -0400

    meta name="version" content="php"

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="version" content="php">
     <style>
         body.offline #link-bar {
             display: none;
