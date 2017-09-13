e8306075e99d42404c30bd6e7e12cb864e434625
William <wjbeckett@gmail.com>
2017-09-13 11:45:06 +1000

    Testing bootstrap loop

--- work files/sample.php
+++ work files/sample.php
@@ -1,4 +1,4 @@
-<?php foreach ($links as $t => $k) { ?>
+<!--<?php foreach ($links as $t => $k) { ?>
 <div class="col-lg-4">
     <a id="requests-status-link" href="" target="_top">
         <img id="requests-service-img" src="" alt="">
@@ -6,4 +6,31 @@
         <p><img id="requests-status-img" src="assets/img/puff.svg"></p>
         <p>Status: <?php urlExists($k); ?></p>
     </a>
+</div>
+-->
+
+<?php
+//Columns must be a factor of 12 (1,2,3,4,6,12)
+$numOfCols = 4;
+$rowCount = 0;
+$bootstrapColWidth = 12 / $numOfCols;
+?>
+<div class="row">
+<?php
+foreach ($rows as $row){
+?>  
+    <?php foreach ($links as $t => $k) { ?>
+	<div class="col-lg-4">
+    	<a id="<?php echo $t ;?>-status-link" href="" target="_top">
+	        <img id="<?php echo $t ;?>-service-img" src="" alt="">
+	        <h4><?php echo $t; ?></h4>
+	        <p><img id="<?php echo $t ;?>-status-img" src="assets/img/puff.svg"></p>
+	        <p>Status: <?php urlExists($k); ?></p>
+    	</a>
+	</div>
+<?php
+    $rowCount++;
+    if($rowCount % $numOfCols == 0) echo '</div><div class="row">';
+}
+?>
 </div>
