ff508e2dcade1650739d4ee65b8a4b702a56b832
William <wjbeckett@gmail.com>
2017-09-14 15:56:56 +1000

    updated check time. removed old js reference

--- index.php
+++ index.php
@@ -26,13 +26,12 @@
     <?php include ('assets/php/check.php') ;?>
     <?php include ('assets/config.php'); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="assets/js/ajax.js"></script>
     <script type="text/javascript">
      $(document).ready(function() {
        $("#statusloop").load('loop.php');
        var refreshId = setInterval(function() {
           $("#statusloop").load('loop.php');
-        }, 3000);
+        }, 5000);
        $.ajaxSetup({ cache: false });
     });
     </script>
