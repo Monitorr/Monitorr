835b9010b447dcafcb161a80e32d8f6ff1ed9741
Sean Vreeland <seanvree@hotmail.com>
2017-09-16 12:26:19 -0700

    Update config.php

--- assets/config.php
+++ assets/config.php
@@ -6,7 +6,7 @@ $config = array(
 // if on Linux, the timezone script will automatically select your timezone
 // For Windows, set the timezone. Default is UTC Time.
 // I.E. ($timezone = 'America/Los_Angeles',)
-	'timezone' => 'UTC',
+    'timezone' => 'UTC',
     'title' => 'YOUR SERVER NAME HERE', // Site Title
     'siteurl' => 'http://localhost', // SITE URL
 );
