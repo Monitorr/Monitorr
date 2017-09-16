8a17713346083a03da61b1d429cbf705c1a484b1
Sean Vreeland <seanvree@hotmail.com>
2017-09-16 12:25:29 -0700

    Added clock/Timezone config

--- assets/config.php
+++ assets/config.php
@@ -3,16 +3,20 @@
 //          EXAMPLE
 //  'dontedit' => 'EDITHERE'
 $config = array(
+// if on Linux, the timezone script will automatically select your timezone
+// For Windows, set the timezone. Default is UTC Time.
+// I.E. ($timezone = 'America/Los_Angeles',)
+	'timezone' => 'UTC',
     'title' => 'YOUR SERVER NAME HERE', // Site Title
-    'siteurl' => 'http://github.com/seanvree/monitorr', // SITE URL
+    'siteurl' => 'http://localhost', // SITE URL
 );
 
 $links = array(
 //             EXAMPLE 
 //  "Nameoflink" => 'http://APP.COM', 
-	"PLEX" => 'https://plex.tv',
-	"Github" =>'https://github.com',
-	"doesn't exist" => 'http://doesntexist.tv',
+"PLEX" => 'http://localhost:32400/web/',
+"OMBI" => 'http://localhost:3579',
+"Sonarr" => 'http://localhost:38084',
 
 );
 ?>
