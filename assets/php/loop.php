a2dffe0ec4339afed69e3e293a8f98aaf9c5fbf4
Sean Vreeland <seanvree@hotmail.com>
2017-09-16 01:48:49 -0700

    Changed service title font

--- assets/php/loop.php
+++ assets/php/loop.php
@@ -4,7 +4,7 @@
     <div class="col-lg-4">
         <a id="<?php echo strtolower($t) ;?>-status-link" href="<?php echo $k ;?>" target="_top">
             <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:80px" alt="">
-	    <h4><?php echo ucfirst($t) ;?></h4>
+	    <br><br><p><strong><?php echo ucfirst($t) ;?></strong></p>
             <p><?php urlExists($k); ?></p>
         </a>
     </div>
