28f781ee4cc5e932373509c8f3eeaddfcbc79406
Sean Vreeland <seanvree@hotmail.com>
2017-09-16 12:19:47 -0700

    Added clock function

--- assets/php/loop.php
+++ assets/php/loop.php
@@ -1,9 +1,39 @@
 <?php include ('check.php') ;?>
 <?php include ('../config.php'); ?>
+<link rel="stylesheet" href="assets/css/main.css">
+
+<div class="col-lg-4">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g>
+                <circle r="100"/>
+                <g id="numbers"/>
+                <g id="ticks"/>
+                <g id="hands">
+                <g id="hour">
+                    <line x1="-2" y1="0" x2="50" y2="0"/>
+                </g>
+                <g id="minute">
+                    <line x1="-3" y1="0" x2="70" y2="0"/>
+                </g>
+                <g id="second">
+                    <line x1="-4" y1="0" x2="95" y2="0"/>
+                </g>
+            </g>
+        </svg>
+        <a>
+        <h4><strong>Server Local DTG:</strong></h4>
+        <div id="timer"></div>
+        </a>
+</div>
+
+<script  src="assets/js/clock.js"></script>
+
 <?php foreach ($links as $t => $k) { ?>
+    
     <div class="col-lg-4">
+    <br>
         <a id="<?php echo strtolower($t) ;?>-status-link" href="<?php echo $k ;?>" target="_top">
-            <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:80px" alt="">
+            <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:170px" alt="">
 	    <br><br><p><strong><?php echo ucfirst($t) ;?></strong></p>
             <p><?php urlExists($k); ?></p>
         </a>
