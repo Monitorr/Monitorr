aa23d9711ae8d694f36928d5b101d7c22a644b4e
Sean Vreeland <seanvree@hotmail.com>
2017-09-16 23:52:53 -0700

    adjust img sizes

--- assets/php/loop.php
+++ assets/php/loop.php
@@ -5,18 +5,18 @@
 <div class="col-lg-4">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
             <g>
-                <circle r="100"/>
+                <circle r="90"/>
                 <g id="numbers"/>
                 <g id="ticks"/>
                 <g id="hands">
                 <g id="hour">
-                    <line x1="-2" y1="0" x2="50" y2="0"/>
+                    <line x1="-2" y1="0" x2="40" y2="0"/>
                 </g>
                 <g id="minute">
-                    <line x1="-3" y1="0" x2="70" y2="0"/>
+                    <line x1="-3" y1="0" x2="65" y2="0"/>
                 </g>
                 <g id="second">
-                    <line x1="-4" y1="0" x2="95" y2="0"/>
+                    <line x1="-4" y1="0" x2="80" y2="0"/>
                 </g>
             </g>
         </svg>
@@ -33,7 +33,7 @@
     <div class="col-lg-4">
     <br>
         <a id="<?php echo strtolower($t) ;?>-status-link" href="<?php echo $k ;?>" target="_top">
-            <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:170px" alt="">
+            <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:110px" alt="">
 	    <br><br><p><strong><?php echo ucfirst($t) ;?></strong></p>
             <p><?php urlExists($k); ?></p>
         </a>
