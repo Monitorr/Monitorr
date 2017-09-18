3ec94e73f88aed141fa051e4f18e35d2de615ba1
Sean Vreeland <seanvree@hotmail.com>
2017-09-18 04:56:08 -0700

    Fixed spacing

--- assets/php/loop.php
+++ assets/php/loop.php
@@ -5,10 +5,9 @@
 <?php foreach ($links as $t => $k) { ?>
     
     <div class="col-lg-4">
-    <br>
         <a id="<?php echo strtolower($t) ;?>-status-link" href="<?php echo $k ;?>" target="_top">
-            <img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:150px" alt="">
-        <br><br><p><strong><?php echo ucfirst($t) ;?></strong></p>
+            <p><img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:85px" alt=""></p>
+            <p><strong><?php echo ucfirst($t) ;?></strong></p>
             <p><?php urlExists($k); ?></p>
         </a>
     </div>
