280167ec59dcea2b2dcb6e28997f669f49d325fe
William <wjbeckett@gmail.com>
2017-09-19 10:40:05 +1000

    added dynamic hyperlinking for online/offline status.

--- assets/php/loop.php
+++ assets/php/loop.php
@@ -3,12 +3,7 @@
 <link rel="stylesheet" href="assets/css/main.css">
 
 <?php foreach ($links as $t => $k) { ?>
-    
-    <div class="col-lg-4">
-        <a id="<?php echo strtolower($t) ;?>-status-link" href="<?php echo $k ;?>" target="_top">
-            <p><img id="<?php echo strtolower($t) ;?>-service-img" src="assets/img/<?php echo strtolower($t) ;?>.png" style="width:85px" alt=""></p>
-            <p><strong><?php echo ucfirst($t) ;?></strong></p>
-            <p><?php urlExists($k); ?></p>
-        </a>
+    <div>
+    	<?php urlExists($k); ?>
     </div>
 <?php } ?>
