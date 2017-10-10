f867c50793191efbc4204cbcba2a8ea8b24fcfeb
Sean Vreeland <seanvree@hotmail.com>
2017-10-10 05:26:50 -0700

    Add files via upload

---
+++ assets/config.php
@@ -0,0 +1,29 @@
+<?php
+// styled by @jonfinley
+//          EXAMPLE
+//  'dontedit' => 'EDITHERE'
+
+$config = array(
+// if on Linux, the timezone script will automatically select your timezone
+// For Windows, set the timezone. Default is UTC Time.
+// I.E. ($timezone = 'America/Los_Angeles',)
+    'timezone' => 'UTC',
+    'title' => 'Monitorr', // Site Title
+    'siteurl' => 'http://localhost', // SITE URL
+);
+
+$links = array(
+//  Add your services here           
+//	EXAMPLE 
+//  "Nameoflink" => 'http://APP.COM', 
+"monitorr" => 'http://localhost/monitorr',
+"PLEX" => 'http://localhost:32400/web/',
+"OMBI" => 'http://localhost:3579',
+"Sonarr" => 'http://localhost:8989',
+"radarr" => 'http://localhost:7878',
+
+// MAX colums is 6 - so anything you add below this line will be on a NEW ROW. 
+
+
+);
+?>
