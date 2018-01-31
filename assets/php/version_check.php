7986751aa58d3f9546a762f280ab3df61fb684f9
Sean Vreeland <seanvree@hotmail.com>
2018-01-31 02:35:31 -0800

    adding update notification

--- assets/php/version_check.php
+++ assets/php/version_check.php
@@ -1,21 +1,28 @@
 <?php
-// check current version number
-require("functions.php");
-// open version file on external server
-$file = fopen ($ext_version_loc, "r");
-$vnum = fgets($file);    
-fclose($file);
-// check users local file for version number
-$userfile = fopen ($vnum_loc, "r");
-$user_vnum = fgets($userfile);    
-fclose($userfile);
-if($user_vnum == $vnum){
-	// data
-	$data = array("version" => 0);
-}else{
-	// data
-	$data = array("version" => $vnum);
-}
-// send the json data
-echo json_encode($data);
+	// check current version number
+	require("functions.php");
+	// open version file on external server
+	$file = fopen ($ext_version_loc, "r");
+	$vnum = fgets($file);    
+	fclose($file);
+	// check users local file for version number
+	$userfile = fopen ($vnum_loc, "r");
+	$user_vnum = fgets($userfile);    
+	fclose($userfile);
+	
+	if($user_vnum == $vnum){
+		// data
+		$data = array("version" => 0);
+	}
+	
+	else{
+		// data
+		$data = array("version" => $vnum);
+	}
+
+	// send the json data
+	echo json_encode($data);
+	//echo "<br>";
+	//echo "remote repo version: $vnum <br />\n";
+	//echo "local repo version: $user_vnum <br />\n";
 ?>
