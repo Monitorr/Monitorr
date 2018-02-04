10a3d79124b947efa771ef5d1361ec2cb40d93e4
Sean Vreeland <seanvree@hotmail.com>
2018-02-04 06:31:33 -0800

    minor code fix for curl_close

--- assets/php/check.php
+++ assets/php/check.php
@@ -85,7 +85,7 @@
                         
                     echo '</a>'; 
                 echo '</div>';
-
+                
                 curl_close($handle);
 
             } 
@@ -151,8 +151,6 @@
                     }
                 
             }
-
-      //curl_close($handle);
 
     };
 
