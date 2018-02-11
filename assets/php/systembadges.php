227564b0114bae148275faa0961272bc70426f36
Sean Vreeland <seanvree@hotmail.com>
2018-02-11 10:37:02 -0800

    added DIV IDs for system stats

--- assets/php/systembadges.php
+++ assets/php/systembadges.php
@@ -2,27 +2,27 @@
 
 <link rel="stylesheet" href="assets/css/main.css">
 
-<div class="col-md-2 col-centered double-val-label">
+<div id="cpu" class="col-md-2 col-centered double-val-label">
   <span class="<?php echo $cpuClass; ?>">CPU</span>
   <span><?php echo $cpuPercent; ?>%</span>
 </div>
 
-<div class="col-md-2 col-centered double-val-label">
+<div id="ram" class="col-md-2 col-centered double-val-label">
   <span class="<?php echo $ramClass; ?>">RAM</span>
   <span><?php echo $ramPercent; ?>%</span>
 </div>
 
-<div class="col-md-2 col-centered double-val-label">
-  <span class="<?php echo $hdClass; ?>">HD used</span>
+<div id="hd" class="col-md-2 col-centered double-val-label">
+  <span class="<?php echo $hdClass; ?>"> HD </span>
   <span><?php echo $freeHD; ?>%</span>
 </div>
 
-<div class="col-md-2 col-centered double-val-label">
-  <span class="primary">ping</span>
-  <span><?php echo $pingTime ;?>ms</span>
-</div>
-
-<div class="col-md-2 col-centered double-val-label">
+<div id="uptime" class="col-md-2 col-centered double-val-label">
   <span class="primary">uptime</span>
   <span><?php echo $total_uptime ;?></span>
+</div>
+
+<div id="ping" class="col-md-2 col-centered double-val-label">
+  <span class="primary">ping</span>
+  <span><?php echo $pingTime ;?>ms</span>
 </div>
