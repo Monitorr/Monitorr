e5d7aae26dc8d7074bf5efcef24e3d8cc881d6e5
jonfinley <accounts@jonathanfinley.net>
2018-02-25 23:34:57 -0500

    Code for PHP Version

--- assets/php/info.php
+++ assets/php/info.php
@@ -112,6 +112,13 @@
             ?>
             <br> <br>
             -	PHP Version
+            <?php
+// prints e.g. 'Current PHP version: 4.1.1'
+echo 'Current PHP version: ' . phpversion();
+
+// prints e.g. '2.0' or nothing if the extension isn't enabled
+echo phpversion('tidy');
+?>
             <br> <br>
             -	Update check here??
              <br> <br>
