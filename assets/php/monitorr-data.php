846685efe2005f6e50546484ca1fdc2e351c61e3
jonfinley <accounts@jonathanfinley.net>
2018-03-07 17:00:41 -0500

    initial data

---
+++ assets/php/monitorr-data.php
@@ -0,0 +1,22 @@
+<?php
+// Data for
+    // Data Directory
+        $str = file_get_contents('assets/config/datadir.json');
+        $json = json_decode( $str, true);
+        $datadir = $json['datadir'];
+    // Login Database
+        $dbfile = $datadir . 'users.db';
+        $db_sqlite_path = $dbfile;
+    // JSON for User Preferences
+        $jsonfileuser = $datadir . 'user_preferences-data.json';
+        $strusers = file_get_contents($jsonfileuser);
+        $jsonusers = json_decode( $strusers, true);
+    // JSON for Site Settings
+        $jsonfilesite = $datadir . 'site_settings-data.json';
+        $strsite = file_get_contents($jsonfilesite);
+        $jsonsite = json_decode( $strsite, true);
+    // JSON for Services
+        $jsonfileservices = $datadir . 'services_settings-data.json';
+        $strservices = file_get_contents($jsonfileservices);
+        $jsonservices = json_decode( $strservices, true);
+?>
