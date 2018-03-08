a00b78cff7d982cd1d5dce7294d63caf178e24e0
Sean Vreeland <seanvree@hotmail.com>
2018-03-07 17:05:17 -0800

    Add files via upload

--- assets/php/timestamp.php
+++ assets/php/timestamp.php
@@ -1,56 +1,41 @@
-<?php // adapted from this website: https://bojanz.wordpress.com/2014/03/11/detecting-the-system-timezone-php/
-    $str = file_get_contents('../data/user_preferences-data.json');
-    $json = json_decode($str, true);
-    $timezone = $json['timezone'];
+<?php 
 
-if (!empty($json['timezone'])) {
+    // adapted from this website: https://bojanz.wordpress.com/2014/03/11/detecting-the-system-timezone-php/
 
-    $str = file_get_contents('../data/user_preferences-data.json');
-    $json = json_decode($str, true);
-    $timezone = $json['timezone'];
-    // echo $timezone;
+    $datafile = '../config/datadir.json';
+
+    include_once ('../config/monitorr-data.php');
 
 
-   // $timezone = $config['timezone']; // set in config.php
-}
+   // $str = file_get_contents('../data/user_preferences-data.json');
+   // $json = json_decode($str, true);
 
 
-    $str = file_get_contents('../data/user_preferences-data.json');
-    $json = json_decode($str, true);
-    $timestandard = $json['timestandard'];
-   // echo $timestandard;
+    
 
-//$timestandard = strtolower($config['timestandard']); // set in config.php
+/*     if (!empty($jsonusers['timezone'])) {
+
+        $str = file_get_contents('../config/_installation/default/user_preferences-data_default.json');
+        $json = json_decode($str, true);
+        $timezone = $json['timezone'];
+            echo $timezone;
+
+    } */
 
 
-/*
-if (is_link('/etc/localtime')) {
-    // Mac OS X (and older Linuxes)
-    // /etc/localtime is a symlink to the
-    // timezone in /usr/share/zoneinfo.
-    $filename = readlink('/etc/localtime');
-    if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
-        $timezone = substr($filename, 20);
-    }
-} elseif (file_exists('/etc/timezone')) {
-    // Ubuntu / Debian.
-    $data = file_get_contents('/etc/timezone');
-    if ($data) {
-        $timezone = $data;
-    }
-} elseif (file_exists('/etc/sysconfig/clock')) {
-    // RHEL / CentOS
-    $data = parse_ini_file('/etc/sysconfig/clock');
-    if (!empty($data['ZONE'])) {
-        $timezone = $data['ZONE'];
-    }
-}*/
+    $timezone = $jsonusers['timezone'];
+    echo $timezone;
+
+    $timestandard = $jsonusers['timestandard'];
+    echo $timestandard;
+
     date_default_timezone_set($timezone);
     $timestamp = time();
     $server_date = date("D | d M <br> Y");
+
 ?>
 
-<div class="dtg">
+    <div class="dtg">
 
 <?php
     if ($timestandard=='True') {
