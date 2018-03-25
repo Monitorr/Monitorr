6fb2ddb3c7d902ad717a64611b5e21d835f5c718
jonfinley <accounts@jonathanfinley.net>
2018-03-25 19:31:23 -0400

    more formatting

--- assets/config/_installation/mkdbajax.php
+++ assets/config/_installation/mkdbajax.php
@@ -25,86 +25,89 @@ $db_sqlite_path = $structure . 'users.db';
 if (file_exists($db_sqlite_path)) { //check if file exists
     rename("users.db", "users.db.old"); //delete file if does exist
 } else {
-    // create new database file / connection (the file will be automatically created the first time a connection is made up)
-    $db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+     $db_type = "sqlite";
 
-    // create new empty table inside the database (if table does not already exist)
-    $sql = 'CREATE TABLE IF NOT EXISTS `users` (
-        `user_id` INTEGER PRIMARY KEY,
-        `user_name` varchar(64),
-        `user_password_hash` varchar(255),
-        `user_email` varchar(64));
-        CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);
-        CREATE UNIQUE INDEX `user_email_UNIQUE` ON `users` (`user_email` ASC);
-    ';
+     $db_sqlite_path = $structure . 'users.db';
 
-// execute the above query
+        // create new database file / connection (the file will be automatically created the first time a connection is made up)
+     $db_connection = new PDO($db_type . ':' . $db_sqlite_path);
 
-$query = $db_connection->prepare($sql);
-$query->execute();
+        // create new empty table inside the database (if table does not already exist)
+     $sql = 'CREATE TABLE IF NOT EXISTS `users` (
+            `user_id` INTEGER PRIMARY KEY,
+            `user_name` varchar(64),
+            `user_password_hash` varchar(255),
+            `user_email` varchar(64));
+            CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);
+            CREATE UNIQUE INDEX `user_email_UNIQUE` ON `users` (`user_email` ASC);
+            ';
 
-echo "<br>";
+        // execute the above query
 
-if (!$query) {
+     $query = $db_connection->prepare($sql);
+     $query->execute();
 
-echo '<div class="reglog">';
-echo '<div id="loginerror">';
-  echo "failed to create user database";
-echo '</div">';
-echo '</div>';
+            echo "<br>";
 
-$fp = fopen('../datadir.json', 'w');
-fwrite( $fp, "Failed to create sqlite database in: $structure");
-fclose($fp);
+        if (!$query) {
 
-rename("../datadir.json", "../datadir.fail.txt");
+            echo '<div class="reglog">';
+                echo '<div id="loginerror">';
+                     echo "failed to create user database";
+                echo '</div">';
+            echo '</div>';
 
-}
+            $fp = fopen('../datadir.json', 'w');
+                fwrite( $fp, "Failed to create sqlite database in: $structure");
+            fclose($fp);
 
-else {
+            rename("../datadir.json", "../datadir.fail.txt");
 
-echo '<div class="reglog">';
+        }
 
-echo '<div id="dbmessagesuccess">';
-  echo "User database creation complete: ";
-  echo realpath($db_sqlite_path);
-echo '</div>';
-  echo "<br>";
+        else {
 
-echo '<div id="dbmessagesuccess">';
-  echo "All required data files succesfully copied to user data dir:";
-      echo "<br>";
-  echo realpath($structure);
-echo '</div>';
-  echo "<br>";
+            echo '<div class="reglog">';
 
-echo '<div id="loginmessage">';
-  echo "Monitorr data directory creation complete. You can now create a user.";
-echo '</div>';
+                echo '<div id="dbmessagesuccess">';
+                     echo "User database creation complete: ";
+                     echo realpath($db_sqlite_path);
+                echo '</div>';
+                     echo "<br>";
 
-echo '</div>';
+                echo '<div id="dbmessagesuccess">';
+                     echo "All required data files succesfully copied to user data dir:";
+                         echo "<br>";
+                     echo realpath($structure);
+                echo '</div>';
+                     echo "<br>";
 
-unlink('../datadir.fail.txt');
+                echo '<div id="loginmessage">';
+                     echo "Monitorr data directory creation complete. You can now create a user.";
+                echo '</div>';
+
+            echo '</div>';
+
+            unlink('../datadir.fail.txt');
 
 
-// Temporary OLD config file removal // CHANGE ME //
+                // Temporary OLD config file removal // CHANGE ME //
 
-$fileold1 = '../../config.php.sample';
+            $fileold1 = '../../config.php.sample';
 
-if(is_file($fileold1)){
+            if(is_file($fileold1)){
 
-rename('../../config.php.sample', '../../config.php.sample.old');
-}
+                rename('../../config.php.sample', '../../config.php.sample.old');
+            }
 
-$fileold2 = '../../config.php';
+            $fileold2 = '../../config.php';
 
-if(is_file($fileold2)){
+            if(is_file($fileold2)){
 
-rename('../../config.php', '../../config.php.old');
-}
+                rename('../../config.php', '../../config.php.old');
+            } 
 
-}
-}
+     }
 }
 
 echo "<br>";
