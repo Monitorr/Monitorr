372faae4cb03c41430646dfadabb057e010bace3
Sean Vreeland <seanvree@hotmail.com>
2018-03-29 17:25:44 -0700

    added auth output

--- assets/php/checkmanual.php
+++ assets/php/checkmanual.php
@@ -23,7 +23,7 @@
     // **  INSERT URL TO CHECK BELOW: ** //
 
 
-    $url = "https://google.com:443";
+    $url = "http://google.com:80";
 
 
     // **  INSERT URL TO CHECK ABOVE ** //
@@ -89,7 +89,10 @@
         if($auth=="1"){
             curl_setopt($handle, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
             curl_setopt($handle, CURLOPT_USERPWD, $creds);
-            echo "Authentication .... Enabled <br> <br />\n";
+            echo "Authentication .... Enabled <br>\n";
+            echo "Creds (first 5)....... ";
+            echo substr($creds, 0, 5); echo "---";
+                echo "<br><br>";
         };
 
         curl_setopt($handle, CURLOPT_FRESH_CONNECT, true);
