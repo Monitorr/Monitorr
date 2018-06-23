17d2bea2df0a3e1428bf5f755765178a4b5ca1e7
Sean Vreeland <seanvree@hotmail.com>
2018-06-23 10:34:47 -0700

    FIX: uptime format

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -323,17 +323,16 @@ function getHDFree3() {
 
 //uptime
 $uptime = shell_exec("cut -d. -f1 /proc/uptime");
-$days = floor($uptime/60/60/24);
+$days = floor($uptime) / 60 / 60 / 24;
 $days_padded = sprintf("%02d", $days);
-$hours = $uptime/60/60%24;
+$hours = round($uptime) / 60 / 60 % 24;
 $hours_padded = sprintf("%02d", $hours);
-$mins = $uptime/60%60;
+$mins = round($uptime) / 60 % 60;
 $mins_padded = sprintf("%02d", $mins);
-$secs = $uptime%60;
+$secs = round($uptime) % 60;
 $secs_padded = sprintf("%02d", $secs);
 // $total_uptime = "$days_padded:$hours_padded:$mins_padded:$secs_padded";
 $total_uptime = "$days_padded:$hours_padded:$mins_padded";
-
 
 // Dynamic icon colors for badges
 
