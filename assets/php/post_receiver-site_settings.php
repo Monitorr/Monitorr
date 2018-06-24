eee258d2a891a6269e5199e63b19306cb6e3ad36
Sean Vreeland <seanvree@hotmail.com>
2018-06-24 02:37:35 -0700

    FIX: Prevent null data posting to json settings files

--- assets/php/post_receiver-site_settings.php
+++ assets/php/post_receiver-site_settings.php
@@ -1,19 +1,36 @@
 <?php
 
-        $str2 = file_get_contents( "../data/datadir.json" );
+    $str2 = file_get_contents( "../data/datadir.json" );
 
-        $json = json_decode( $str2, true);
+    $json = json_decode( $str2, true);
 
-        $datadir = $json['datadir'];
+    $datadir = $json['datadir'];
 
-        echo $datadir;
+    //echo $datadir;
 
-        $jsonpath = $datadir . 'site_settings-data.json';
+    $jsonpath = $datadir . 'site_settings-data.json';
 
-        echo $jsonpath;
+    //echo $jsonpath;
+
+    if (isset($_POST["rfsysinfo"])) {
+
+        echo "POST detected.";
+            echo "<br>";
+        echo  "rfsysinfo is set. Writing values to json settings file.";
+            echo "<br>";
 
         $fp = fopen($jsonpath, 'w');
             fwrite($fp, json_encode($_POST));
         fclose($fp);
+    }
+    
+    else{
+        
+        echo "<script type='text/javascript'>";
+            echo "console.log('POST not detected. NOT writing values to json settings file.');";
+        echo "</script>";
+
+        echo "POST not detected. NOT writing values to json settings file.";
+    }
 
 ?>
