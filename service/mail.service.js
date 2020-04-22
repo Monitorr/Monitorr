6e6dc9294640878319925a53a0db3edb901edc25
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2020-04-22 14:06:40 +0100

    some mail additions

--- service/mail.service.js
+++ service/mail.service.js
@@ -2,14 +2,18 @@ import fs from 'fs';
 import nodemailer from 'nodemailer';
 
 let mailSettings = JSON.parse(fs.readFileSync('./config/mail.json'));
-const transporter = nodemailer.createTransport(mailSettings);
 
 export const enabled = mailSettings.enabled;
 
 export const sendMail = ({ subject, text }) => {
-    const sendOpts = { from: mailSettings.from, to: mailSettings.to, subject, text };
-    transporter.sendMail(sendOpts, (err, info) => {
-        if (err) console.error(err);
-        else console.log('Mail Sent %s', info.response);
-    });
-}
+    if (enabled) {
+        const transporter = nodemailer.createTransport(mailSettings);
+        const sendOpts = { from: mailSettings.from, to: mailSettings.to, subject, text };
+        transporter.sendMail(sendOpts, (err, info) => {
+            if (err) console.error(err);
+            else console.log('Mail Sent %s', info.response);
+        });
+    } else {
+        throw new Error('Mail is not enabled');
+    }
+};
